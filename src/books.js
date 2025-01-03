0ba65fc943e4c66d157cecfc1cab129636be1159
camqvan <46548868+camqvan@users.noreply.github.com>
2025-01-03 13:06:06 -0800

    first commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+export const books = [
+  {
+    author: 'Rebecca Yarros',
+    title: `Onyx Storm (Deluxe Limited Edition) (The Empyrean, 3)`,
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Mel Robbins',
+    title: `The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About`,
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'James Clear',
+    title: `Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones`,
+    img: img3,
+    id: 3,
+  },
+];
